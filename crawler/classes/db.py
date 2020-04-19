dd6159ce7f4addabed0aa79610e0b5fbc047baaf
Jack Casey <jackdcasey@gmail.com>
2020-04-19 16:23:48 -0700

    Specify region for DynamoDB connection

--- crawler/classes/db.py
+++ crawler/classes/db.py
@@ -7,7 +7,7 @@ def WriteToDatabase(table: str, data: dict) -> None:
     print(f"Writing to table: {table}")
     print(data)
 
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb', region='us-west-2')
     table = dynamodb.Table(table)
 
     table.put_item(
